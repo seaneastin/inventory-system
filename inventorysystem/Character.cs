652190f0ad769fa1be8c79a8a863e80ff54b9093
Sean Eastin <s198013@students.aie.edu>
2019-08-26 11:23:02 -0500

    added character class and a lot of things

---
+++ inventorysystem/Character.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventorysystem
+{
+    class Character
+    {
+        private string _name = "";
+        private int _xp = 0;
+        private int _level = 1;
+        private int[] _requiredXP = { 100, 300, 600, 1000 }; //required xp for leveling up
+
+
+
+        public Character(string name)
+        {
+            _name = name;
+        }
+
+        public string Name()
+        {
+            return _name;
+        }
+        public void Print()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine("level " + _level);
+            Console.WriteLine("experience " + _xp);
+        }
+        public int Experience 
+        {
+            get
+            {
+                return _xp;
+                
+            }
+            set
+            {
+                _xp = value;
+                Console.WriteLine(_name + " gained experience and now has " + _xp);
+                if (_level <= _requiredXP.Length)
+                    {
+                    if (_xp >= _requiredXP[_level - 1]) //subtracts 1 to make sure taht when you level up it uses the currrect slot
+                    {
+                        _level++;
+                        Console.WriteLine(_name + "s level increased to " + _level + "!");
+                    }
+                }
+            }
+        }
+    }
+}
--- inventorysystem/Program.cs
+++ inventorysystem/Program.cs
@@ -10,8 +10,75 @@ namespace inventorysystem
     {
         static void Main(string[] args)
         {
-            inventory inventory = new inventory(); //not sure what this does will ask the teacher
-            inventory.Menu(); //opens a menu i think
+            //*inventory inventory = new inventory(); //not sure what this does will ask the teacher
+            //* inventory.Menu(); //opens a menu i think
+
+            Character player = new Character("Sean");
+            player.Print();
+            Console.WriteLine(player.Name());
+            Console.ReadLine();
+            Character dylan = new Character("dylan");
+            dylan.Print();
+
+            player.Experience = 30;
+            player.Experience = player.Experience + 50;
+            player.Experience++;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience += 40;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            player.Experience++;
+            dylan.Experience += 1;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience += 100;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            dylan.Experience++;
+            int[] testArray = new int[4];
+            testArray[0] = 1;
+            testArray[1] = 3;
+            testArray[2] = 5;
+            testArray[3] = 7;
+
+            int[] testArray2 = { 2, 4, 6, 8 };
+            string[] stringarray = new string[3];
+            Character[] party = { player, dylan, new Character("Like") }; //ask teacher about arrays
+
+
+            Console.ReadKey();
         }
     }
 }
